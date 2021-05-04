4d56c6ef6b1de9eda9e417fe03d462bd224ae845
Mrunal401 <mrunalmpatil1@gmail.com>
2021-05-04 10:52:36 +0530

    login, sign up, otp, side bar UIs are ready

---
+++ app/src/main/java/com/example/womensafety/Activities/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.womensafety.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.womensafety.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int SPLASH_SCREEN_TIMEOUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}
